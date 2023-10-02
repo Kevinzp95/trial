33d98ae4dfa6a2279253b3e0d3ad6a37b32dcb48
Alex <a.kuznetsov23@gmail.com>
2023-10-02 14:15:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// function calculateTotal() {
+//   const pizza1Quantity = parseInt(document.getElementById("pizza1").value);
+//   const pizza2Quantity = parseInt(document.getElementById("pizza2").value);
+//   const pizza3Quantity = parseInt(document.getElementById("pizza3").value);
+//   const pizza4Quantity = parseInt(document.getElementById("pizza4").value);
+
+//   const pizza1Price = 300; // Стоимость пиццы 1
+//   const pizza2Price = 400; // Стоимость пиццы 2
+//   const pizza3Price = 350; // Стоимость пиццы 3
+//   const pizza4Price = 450; // Стоимость пиццы 4
+
+//   const totalCost =
+//     pizza1Quantity * pizza1Price +
+//     pizza2Quantity * pizza2Price +
+//     pizza3Quantity * pizza3Price +
+//     pizza4Quantity * pizza4Price;
+
+//   document.getElementById("totalCost").textContent = totalCost;
+// }
